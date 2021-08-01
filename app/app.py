3cefb01c35fda7a5055027ecff0035bc0979305d
jasneekchugh <chughjasneek1096@gmail.com>
2021-08-01 00:49:44 -0400

    Task: Created initial skeleton for the application

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,
+    instance_relative_config=False,
+    template_folder="templates",
+    static_folder="static"
+            )
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
